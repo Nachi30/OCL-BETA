33cbda75289963046d197b406d1880ad00307f4e
Gamesensei <30955644+Nachi30@users.noreply.github.com>
2019-10-04 20:44:22 -0300

    Update ocl-physics.js

--- OCL BETA 2/OCL project TEMPLATE/ocl/ocl-physics.js
+++ OCL BETA 2/OCL project TEMPLATE/ocl/ocl-physics.js
@@ -1,3 +1,12 @@
 function physics(){
-    
+        this.checkCollide = function(x, y, x2, y2, ancho1, largo1, ancho2, largo2){
+            collision = null;
+            if (x < x2 + ancho1 &&
+                x + ancho1 > x2 &&
+                y < y2 + largo2 &&
+                largo1 + y > y2) {
+                    collision = true
+                    return collision;
+         }
+    }
 }
